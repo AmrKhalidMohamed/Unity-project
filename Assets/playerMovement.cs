23dc016fd7e1bb1348c83b37594012abf59b7b4d
Amr Khaled <amrkfawzy@gmail.com>
2024-05-12 02:12:12 +0300

    Added score

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Text = TMPro.TextMeshProUGUI;
 
 public class playerMovement : MonoBehaviour
 {
+    public Text scoreText;
+    private static int score = 0;
+
+
     public float moveSpeed = 5f;
     public float jumpForce = 16f;
     private Rigidbody2D rb;
@@ -22,6 +29,8 @@ public class playerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+
+        scoreText.text = "Score: " + score;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -37,16 +46,27 @@ public class playerMovement : MonoBehaviour
 
     void Die()
     {
-        
+
         deathScreen.SetActive(true);
-        
+
         GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
+
     void Win()
     {
-        
         winScreen.SetActive(true);
-        
         GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        score++;
+
+
+        // Restart the current scene after a delay
+        Invoke("RestartGame", 2f); // 2 seconds delay
+    }
+
+    void RestartGame()
+    {
+        // Reload the current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
